a22c1dcd95d4792287a6257fe814f66b5236c957
Natanael Arndt <arndtn@gmail.com>
2022-08-02 10:14:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# MusicXML to Flipper Music Format
+
+This script reads a (not compressed) [MusicXML](https://www.w3.org/2021/06/musicxml40/) ([Wikipedia](https://en.wikipedia.org/wiki/MusicXML)) file and transforms it to the [Flipper Music Format](https://github.com/Tonsil/flipper-music-files) which can be executed on the [Flipper Zero](https://flipperzero.one/).
+
+This allows to compose your music with graphical tools like [MuseScore](https://en.wikipedia.org/wiki/MuseScore) and play the music on the Flipper.
+
+## TODO
+
+### Ties
+
+```
+<notations>
+ <tied type="stop"/>
+ <tied type="start"/>
+</notations>
+```
---
+++ musicxml2fmf.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import click
+from lxml import etree
+
+downsteps = {
+    "C": ("B", -1),
+    "D": ("C", 0),
+    "E": ("D", 0),
+    "F": ("E", 0),
+    "G": ("F", 0),
+    "A": ("G", 0),
+    "B": ("A", 0)
+}
+
+durations = {
+    1: (8, ""),
+    2: (4, ""),
+    3: (4, "."),
+    4: (2, ""),
+    6: (2, "."),
+    8: (1, "")
+}
+
+
+@click.command()
+@click.option("--input")
+@click.option("--output")
+def convert(input, output):
+    with open(output, 'w') as o:
+        o.write("""Filetype: Flipper Music Format
+Version: 0
+BPM: 90
+Duration: 8
+Octave: 5
+Notes: """)
+        tree = etree.parse(input)
+        res = tree.xpath("//score-partwise/part/measure/note")
+        for note in res:
+            duration = note.xpath("duration")[0].text
+            fduration, fdot = durations[int(duration)]
+            if note.xpath("rest"):
+                o.write(f"{fduration}P, ")
+                continue
+            alter = False
+            if note.xpath("pitch/alter"):
+                alter = int(note.xpath("pitch/alter")[0].text)
+            step = note.xpath("pitch/step")[0].text
+            octave = int(note.xpath("pitch/octave")[0].text)
+            sharp = ""
+            if alter:
+                if alter < 0:
+                    step, octaveShift = downsteps[step]
+                    octave = octave + octaveShift
+                sharp = "#"
+            o.write(f"{fduration}{step}{sharp}{octave}{fdot}, ")
