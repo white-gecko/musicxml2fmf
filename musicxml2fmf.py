cc576b305791e45e501950adf8b01fc76fd536e3
Natanael Arndt <arndtn@gmail.com>
2022-08-03 10:42:35 +0200

    Add default values to cli and add object model for notes and rests

--- musicxml2fmf.py
+++ musicxml2fmf.py
@@ -24,35 +24,63 @@ durations = {
 
 filetypeHeader = """Filetype: Flipper Music Format
 Version: 0
-BPM: 90
-Duration: 8
-Octave: 5
+BPM: {bpm}
+Duration: {duration}
+Octave: {octave}
 Notes: """
 
 
+class Note:
+    def __init__(self, duration=8, step="C", sharp="", octave=5, dot=""):
+        self.duration = duration
+        self.step = step
+        self.sharp = sharp
+        self.octave = octave
+        self.dot = dot
+
+    def __str__(self):
+        return f"{self.duration}{self.step}{self.sharp}{self.octave}{self.dot}"
+
+
+class Rest:
+    def __init__(self, duration=8, dot=""):
+        self.duration = duration
+        self.dot = dot
+
+    def __str__(self):
+        return f"{self.duration}P{self.dot}"
+
+
 @click.command()
-@click.option("--input")
-@click.option("--output")
-def convert(input, output):
+@click.option("--input", help="File to read the MusicXML from (suffix: '.musicxml').")
+@click.option("--output", help="File to write the Flipper Music to (suffix: '.fmf').")
+@click.option("--bpm", default=120, help="Beats per minute for the piece (default: 120).")
+@click.option("--duration", default=8, help="Default duration of a tone (default: 8).")
+@click.option("--octave", default=5, help="Default octave of a note (default: 5).")
+def convert(input, output, bpm, duration, octave):
+    flipperNotes = []
+    tree = etree.parse(input)
+    musicXmlNotes = tree.xpath("//score-partwise/part/measure/note")
+    for noteTag in musicXmlNotes:
+        noteDuration = noteTag.xpath("duration")[0].text
+        flipperDuration, dot = durations[int(noteDuration)]
+        if noteTag.xpath("rest"):
+            flipperNotes.append(Rest(flipperDuration))
+            continue
+        alter = False
+        if noteTag.xpath("pitch/alter"):
+            alter = int(noteTag.xpath("pitch/alter")[0].text)
+        step = noteTag.xpath("pitch/step")[0].text
+        octave = int(noteTag.xpath("pitch/octave")[0].text)
+        sharp = ""
+        if alter:
+            if alter < 0:
+                step, octaveShift = downsteps[step]
+                octave = octave + octaveShift
+            sharp = "#"
+        note = Note(flipperDuration, step, sharp, octave, dot)
+        flipperNotes.append(note)
+
     with open(output, 'w') as o:
-        o.write(filetypeHeader)
-        tree = etree.parse(input)
-        res = tree.xpath("//score-partwise/part/measure/note")
-        for note in res:
-            duration = note.xpath("duration")[0].text
-            fduration, fdot = durations[int(duration)]
-            if note.xpath("rest"):
-                o.write(f"{fduration}P, ")
-                continue
-            alter = False
-            if note.xpath("pitch/alter"):
-                alter = int(note.xpath("pitch/alter")[0].text)
-            step = note.xpath("pitch/step")[0].text
-            octave = int(note.xpath("pitch/octave")[0].text)
-            sharp = ""
-            if alter:
-                if alter < 0:
-                    step, octaveShift = downsteps[step]
-                    octave = octave + octaveShift
-                sharp = "#"
-            o.write(f"{fduration}{step}{sharp}{octave}{fdot}, ")
+        o.write(filetypeHeader.format(bpm=bpm, duration=duration, octave=octave))
+        o.write(", ".join(str(n) for n in flipperNotes))
